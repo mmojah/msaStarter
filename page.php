bc72c1251eeca75112516762a08a345f353ca4ad
root <root@aegir.local>
2018-01-16 11:03:33 -0300

    hola

--- page.php
+++ page.php
@@ -13,7 +13,7 @@
  */
 
 get_header(); ?>
-<div id="content" class="site-content">
+<div id="content" class="site-content class">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
